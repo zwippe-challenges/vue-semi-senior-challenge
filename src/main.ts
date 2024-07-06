76397f23f90d42e97e8f3b66b00367b02232fe4d
DanielQDev <danielquingaluisadev@gmail.com>
2024-07-06 13:07:14 -0500

    Initial commit.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './index.css'
+import './styles/index.css'
 
 const app = createApp(App)
 
